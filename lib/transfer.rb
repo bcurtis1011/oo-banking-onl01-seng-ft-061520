6acaf7608fddfbc369651de4416ce2dc2c176abb
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-18 17:01:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,14 +14,14 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-    sender.balance -= amount
-    receiver.balance += amount
-    self.status = "complete"
-  else
-    reject_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "complete"
+    else
+      reject_tranfer
+    end
   end
-end
   
   def reject_tranfer
     self.status = "rejected"
